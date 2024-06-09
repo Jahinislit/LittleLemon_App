597dd1067760c98568fb3f666ed0afbc5722b4b8
JahinisLit <jahinhalderexiting@gmail.com>
2024-06-09 22:47:42 +0530

    Initial Commit

---
+++ app/src/main/java/com/littlelemon/littlelemonproject/composables/NavigationComposable.kt
@@ -0,0 +1,33 @@
+package com.littlelemon.littlelemonproject.composables
+
+import android.content.Context
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import com.littlelemon.littlelemonproject.navigation.Home
+import com.littlelemon.littlelemonproject.navigation.Onboarding
+import com.littlelemon.littlelemonproject.navigation.Profile
+
+@Composable
+fun NavigationComposable(context: Context,navController: NavHostController) {
+
+    val sharedPreferences = context.getSharedPreferences("Little Lemon", Context.MODE_PRIVATE)
+    var startDestination = Onboarding.route
+
+    if (sharedPreferences.getBoolean("userRegistered", false)) {
+        startDestination = Home.route
+    }
+
+    NavHost(navController = navController, startDestination = startDestination){
+        composable(Onboarding.route){
+            Onboarding(context, navController)
+        }
+        composable(Home.route){
+            Home(navController)
+        }
+        composable(Profile.route){
+            Profile(context, navController)
+        }
+    }
+}
